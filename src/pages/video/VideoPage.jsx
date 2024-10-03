87e4c341e3ef51c2ea0ecaf935f1042052d2a766
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-10-03 14:06:39 +0400

    Added back button in VideoPage component

--- src/pages/video/VideoPage.jsx
+++ src/pages/video/VideoPage.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./VideoPage.css"
+import "./VideoPage.css";
 import { useParams, useNavigate } from "react-router-dom";
 import YouTubeVideo from "../../components/youTubeVideo/YouTubeVideo";
 import { videos } from "../../config/videoData";
@@ -45,20 +45,35 @@ const VideoPage = () => {
         setModalIsOpen(false);
     };
 
+    const goToHomePage = () => {
+        navigate("/");
+    };
+
     return (
-        <div
-            style={{ height: "100vh", display: "flex", flexDirection: "column" }}
-        >
+        <div style={{ height: "100vh", display: "flex", flexDirection: "column" }}>
             <div style={{ flex: 1, position: "relative" }}>
+                <div className="video-header">
+                    <button onClick={goToHomePage}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                             className="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+                            <path fillRule="evenodd"
+                                  d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
+                        </svg>
+                    </button>
+                </div>
+
                 <YouTubeVideo videoId={id} />
 
-                <button onClick={openModal} className={'top-btn'}>
+                <button onClick={openModal} className="top-btn">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                          className="bi bi-arrow-up-circle" viewBox="0 0 16 16">
                         <path fillRule="evenodd"
                               d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707z"/>
                     </svg>
                 </button>
+
+                <div className="btn-block">
+                </div>
             </div>
 
             <VideoModal
